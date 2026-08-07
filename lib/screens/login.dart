8b4c590188224d1904917dab7c42eadbcceb1b47
mustawtfa <mustawtfa@gmail.com>
2026-08-07 21:43:25 +0300

    fix: change SingleTickerProviderStateMixin to TickerProviderStateMixin

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -19,7 +19,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
