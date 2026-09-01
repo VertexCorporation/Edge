02cdd1b255ccfad398664451a0afb695f8a06c6f
ZeroHighx <eymencanorman09@gmail.com>
2026-09-01 19:11:18 +0300

    chore: revert version back to 1.0.31+31

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
 /// Keep in sync with pubspec.yaml version.
-const String kAppVersion = '1.0.33';
-const String kAppBuild = '33';
+const String kAppVersion = '1.0.31';
+const String kAppBuild = '31';
