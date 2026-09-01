cdae01d00aef1b89e467cf1f6bfe92126d8d88ec
ZeroHighx <eymencanorman09@gmail.com>
2026-09-01 17:01:56 +0300

    chore: bump version to 1.0.30+30

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
 /// Keep in sync with pubspec.yaml version.
-const String kAppVersion = '1.0.29';
-const String kAppBuild = '29';
+const String kAppVersion = '1.0.30';
+const String kAppBuild = '30';
